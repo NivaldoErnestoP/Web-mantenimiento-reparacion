576f42f43b0673f332ff43e80f083aea2d3122d1
Nivaldo <89329434+NivaldoErnestoP@users.noreply.github.com>
2021-10-08 04:18:56 -0400

    Delete app2.js

--- app2.js
+++
@@ -1,8 +0,0 @@
-var navBotom = document.querySelector("#navBot");
-var colap_div = document.querySelector("#colap");
-
-navBotom.addEventListener("click",()=>{
-    colap_div.classList.toggle("collapse"); 
-
-});
-console.log(colap_div);
